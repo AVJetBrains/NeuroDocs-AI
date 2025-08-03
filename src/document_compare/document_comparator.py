05ad847766f6791c58893f75f36ecf0d8fc96d9f
AVJetBrains <weaakash@gmail.com>
2025-08-03 16:54:52 +0530

    RAG Implementation and single doc chat implemented

--- src/document_compare/document_comparator.py
+++ src/document_compare/document_comparator.py
@@ -15,7 +15,7 @@ class DocumentComparatorLLM:
         self.log = CustomLogger().get_logger(__name__)
         self.loader = ModelLoader()
         self.llm = self.loader.load_llm()
-        self.parser = JsonOutputParser(pydantic_object=SummaryReponse)
+        self.parser = JsonOutputParser(pydantic_object=SummaryResponse)
         self.fixing_parser = OutputFixingParser.from_llm(parser=self.parser, llm=self.llm)
         self.prompt = PROMPT_REGISTRY["document_comparision"]
         self.chain = self.prompt | self.llm | self.parser
@@ -30,9 +30,9 @@ class DocumentComparatorLLM:
                 "combined_docs": combined_docs,
                 "format_instruction": self.parser.get_format_instructions()
             }
-            self.log.info("Starting document comparison", inputs=inputs)
+            self.log.info("Invoking document comparison LLM chain")
             response = self.chain.invoke(inputs)
-            self.log.info("Document comparison completed", response=response)
+            self.log.info("Chain invoked successfully", response_preview=str(response)[:200])
             return self._format_response(response)
 
             
